c466f54226a1819ba10c7e2b9187be31dcfb8967
David Park <dpark4657@gmail.com>
2017-11-26 19:24:16 -0800

    completes part 7 - routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,6 +7,11 @@ import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
